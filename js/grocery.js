2901c9a99220bb05ede626b12b1615d831f67109
Wils Iglesias <wiglesias83@gmail.com>
2021-11-01 18:45:00 +0100

    Add product to cart

--- js/grocery.js
+++ js/grocery.js
@@ -80,7 +80,14 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let i = 0; i < products.length; i++) {
+        if (id === products.indexOf(products[i])) {
+            // 2. Add found product to the cartList array
+            cartList.push(products[i]);
+        }
+    }
+    console.log(cartList);
+    
 }
 
 // Exercise 2
