5e68fb45c5f16134f6c644995edacf2df420c5f5
Wils Iglesias <wiglesias83@gmail.com>
2021-11-04 20:11:00 +0100

    Implemeted validate form firstName and phone field

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,33 @@
 // Get the input fields
 var password = document.querySelector(".password");
 var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var firtsName = document.querySelector('.name');
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorName = document.getElementById('errorName');
+var errorPhone = document.getElementById('errorPhone');
 
 // Exercise 8
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
+    let letters = /^[a-zA-Z]$/;
+
+    if (firtsName.value.length < 3) {
+        firtsName.classList.add('invalid');
+        errorName.innerHTML = "Name must be at least 3 characters";
+        errorName.style.display = 'inline';
+    } else {
+        firtsName.classList.add('invalid');
+        errorName.style.display = 'inline';
+    }
+
+    if(phone.value.length < 3){
+        phone.classList.add("invalid");
+        errorPhone.innerHTML = "Phone must be at least 3 numbers"
+        errorPhone.style.display="inline";
+    } else {
+        phone.classList.remove("invalid");
+        errorPhone.style.display="none";
+    }
 }
